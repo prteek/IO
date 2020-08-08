7aa6c9b3530cf571542c2977b6b563a8c1217ac9
Prateek <prateekpatel.in@gmail.com>
2020-08-08 15:56:41 +0100

    blog files (dummies)

---
+++ src/blogs/blog_home.py
@@ -0,0 +1,23 @@
+import streamlit as st
+from src.blogs import blog_1 as b1
+from src.blogs import blog_2 as b2
+
+def run():
+    
+    class home_page():
+        def __init__(self):
+            return None
+
+        def run(self):
+            st.title('Blog home')
+            st.write('Home of Chatties')
+
+    home = home_page()
+    
+    projects = {'Home':home,
+        'blog 1':b1, 
+     'blog 2':b2}
+
+    page = st.sidebar.selectbox(label='Blogs', options=list(projects.keys()))
+
+    projects[page].run()
