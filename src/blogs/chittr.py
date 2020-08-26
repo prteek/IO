2f73c39a4cf7433bc30384ae357fbe41276d120e
prateek <iprtk@icloud.com>
2020-08-26 22:45:26 +0100

    added new posts and Chittr

---
+++ src/blogs/chittr.py
@@ -0,0 +1,29 @@
+import streamlit as st
+def run():
+    st.title("Chittr")    
+    st.markdown("""##""")
+
+    #4
+    st.markdown("""The tougher the question the easier it is dismissed. When we be facing an easy dismissal, it be worth asking “was the problem even well understood?”
+    
+---
+""")
+    
+    #3
+    html_string = """<blockquote class="twitter-tweet"><p lang="en" dir="ltr">So I guess <a href="https://twitter.com/hashtag/data?src=hash&amp;ref_src=twsrc%5Etfw">#data</a> is knowledge but <a href="https://twitter.com/hashtag/code?src=hash&amp;ref_src=twsrc%5Etfw">#code</a> is wisdom 🤔 <a href="https://t.co/bzYppH6bQo">https://t.co/bzYppH6bQo</a></p>&mdash; Prateek (@prateekpatel_in) <a href="https://twitter.com/prateekpatel_in/status/1284201607488569348?ref_src=twsrc%5Etfw">July 17, 2020</a></blockquote> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+    
+---
+"""
+    st.markdown(html_string, unsafe_allow_html=True)
+    
+    #2
+    html_string = """<blockquote class="twitter-tweet"><p lang="en" dir="ltr">Is it cannibalism to quit Task manager from Task manager ? 🤔</p>&mdash; Prateek (@prateekpatel_in) <a href="https://twitter.com/prateekpatel_in/status/1283337696669294592?ref_src=twsrc%5Etfw">July 15, 2020</a></blockquote> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+    
+---
+"""
+    st.markdown(html_string, unsafe_allow_html=True)
+    
+    #1
+    st.markdown("""Is it just me or the [#AvengersEndgame](https://twitter.com/search?q=%23avengersendgame&src=typed_query) storyline is like a typical [@github](https://twitter.com/github) project evolution ?🤷🏻
+ """)
+    
---
+++ src/blogs/relativity.py
@@ -0,0 +1,48 @@
+import streamlit as st
+def run():
+    st.title('Logical notes on Relativity')
+    
+    st.markdown("""Consider Newton’s laws of motion and the experiments of motion relevant to it. The inertial frame of reference defined as a frame which moves at constant or 0 speed relative to a reference inertial frame (non-accelerating ).
+
+If one drops a ball on standing on a train platform the ball hits their feet.  
+Alternately if they drop the ball on a moving train (moving at a constant speed relative to the platform) the ball again hits their feet. Practically the experiment gives the same result irrespective of which frame it was performed in. Also an interesting thing is it does not matter how fast the train is moving in this experiment. Conversely saying that based on the experiment it cannot be determined how fast the train was moving as the results of experiment do not change in that respect.  
+
+So, Newtons laws hold consistent in different “inertial” frames of reference.  
+Can it also be true for other laws of Nature ? 🧐  
+
+Let’s consider Maxwell’s law which states that the speed of light in vacuum is a constant (c = 300,000,000 m/s). Experiments have proven that the speed of light when measured from different inertial frames does not change i.e. it should be c = 300,000,000 m/s.  
+Imagine 2 observers, first observer A stationary and other observer B moving with half the speed of light. Also imagine a wave of light W moving in the same direction as the moving observer B.
+If at the start of experiment, A, B and W are at the same point and thereafter B (at c/2 m/s) and W (at c m/s) start moving. At every second A and B observe one another and the light wave W and note the distances of each, we immediately begin to see a flaw.  
+The observers will note the distance to each other (say AB) as same which is logical (distance = c/2*time). But, because for both of them light W moves at a constant speed ‘c’ both can observe light to cover same distance relative to themselves (distance = c*time) even though the observers are moving relative to each other at the same time.  
+
+This inconsistency can arise from either of the below 3 reasons:  
+
+1. Maxwell’s law are not consistent in different “inertial” frame of references A and B.  
+2. The measure of distance for A and B are different hence they report same distance for wave W despite B is moving and A is stationary.  
+3. The measure of time for A and B are different and they note down the distances readings at different times from the start of experiment.  
+
+If 1. is true then the laws of Newton that hold consistent for different inertial frames are fundamentally different from laws of electricity and magnetism which do not behave consistently across different inertial frames of reference. That means if one were to perform experiments related to electricity and/or magnetism on the moving train then they would derive different results.  
+Also it should be possible to ascertain the speed of the train (frame of reference) from the result of experiments. However, this is not seen to be true from the experiments of electricity and magnetism. So 1 cannot be the reason and Maxwell’s laws must hold true in different inertial frames of references.  
+
+Einstein puts this ideas into 2 postulates of relativity:  
+a. The laws of nature have identical forms in all inertial frames.  
+b. The speed of light in vacuum c = 300,000,000 m/s has the same value if observed from any inertial frame of reference.  
+
+Now consider point 2: If one experiments with two bars of same length (let B1 and B2) moving perpendicular to their lengths. Now if B2 (while moving perpendicular to its length) moves such that it passes over B1 which is stationary and an observer observes from B1, then there are two possibilities that can result if point 2 is true (but only one of them can be true at a time not both):  
+
+1. The length of bar that is observed as moving gets shorter because it is moving.  
+2. The length of bar that is observed as moving gets longer because it is moving.  
+
+For an observer on B1 considering i is true, the bar B2 is moving and must become shorter.  
+For the same case i being true if an observer observes B1 from B2 then the bar B1 is moving. So, it must become shorter.  
+But B1 and B2 both cannot become shorter. So the point 2 must be false.
+
+    """)
+    
+    st.markdown(""" ###
+    ---
+    [Prateek](https://www.linkedin.com/in/prteek/ "LinkedIn")  
+    [Repository](https://github.com/prteek/IO/ "Github")
+
+    """)
+    
