caf16e8ed17c6b8a124b77a245dded35af565ae3
prateek <iprtk@icloud.com>
2020-08-26 22:44:33 +0100

    renamed blog post files≈

--- src/blogs/blog_home.py
+++ src/blogs/blog_home.py
@@ -1,22 +1,14 @@
 import streamlit as st
-from src.blogs import blog_1 as b1
-from src.blogs import blog_2 as b2
+from src.blogs import chittr as chit
+from src.blogs import relativity as rltv
+from src.blogs import dillema_of_autonomous_vehicles as doav
 
 def run():
     
-    class home_page():
-        def __init__(self):
-            return None
-
-        def run(self):
-            st.title('Blog home')
-            st.write('Feeling Chatty')
-
-    home = home_page()
-    
-    projects = {'Home':home,
-        'blog 1':b1, 
-     'blog 2':b2}
+    projects = {'Home':chit,
+                'Logical notes on relativity':rltv,
+                'Dillema of Autonomous Vehicles': doav,
+               }
 
     page = st.sidebar.selectbox(label='Blogs', options=list(projects.keys()))
 
