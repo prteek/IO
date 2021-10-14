37b57387c950af593988210becbfa198977ee118
Prateek Patel <MZ9UTE@alsn.com>
2021-10-14 14:38:46 +0100

    added gpu model fitting

--- src/blogs/blog_home.py
+++ src/blogs/blog_home.py
@@ -1,11 +1,11 @@
 import streamlit as st
 from src.blogs import chittr as chit
+from src.blogs import fit500 as fit500
 
 def run():
     
     projects = {'Home':chit,
-               }
+               'Fit 500 models on GPU':fit500}
 
-#     page = st.sidebar.selectbox(label='Blogs', options=list(projects.keys()))
-    page = 'Home'
+    page = st.sidebar.selectbox(label='Blogs', options=list(projects.keys()))
     projects[page].run()
