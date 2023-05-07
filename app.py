391fc630f18bde715d3289bd3352f94d3f4e2cfd
prateek <prateek.patel@bricklane.com>
2023-05-07 19:12:41 +0100

    some changes

--- app.py
+++ app.py
@@ -13,19 +13,21 @@ class home_page():
         st.title('IO')
         st.write("""Hi ! I am your just another friendly neighbourhood Data-Scientist.  
 Similar to your other friendly neighbourhood costumed crusaders, I believe that "with great power comes great responsibility".  
-That is why I rely on sound logic and principles of physics and math for designing and analysing stuff that matter.  
+So I rely on sound logic and principles of physics and math for designing and analysing stuff that matter.  
 In the past I have worked with Automotive companies like:
 * Suzuki
 * Jaguar Land Rover
 * Ford
 * Allison Transmission
+* Cloud Cycle 
+* Bricklane
 
-To help them make sense of the customer usage (and powertrain systems) data that they have, to aid in reasearch and development of new products, particularly Hybrid Electric vehicles.  
+On problems ranging from track testing, Hybrid electric powertrain design, vehicle monitoring system, concrete quality degradation
+and real estate behaviour modelling.
 
-I have deep interest in Statistics, Machine learning and Python and I occassionally dabble in Astrophysics and Photography.  
+I have deep interest in Statistics, Machine learning and Python and I occasionally dabble in Astrophysics and Photography.  
 
 If you'd like to get in touch, please use the details below:
-
 
         """)
         
