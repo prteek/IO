fc03cecf3ce3add6a030f08f4c5f884feaadb1f4
prateek <prateek.patel@bricklane.com>
2023-05-14 19:59:18 +0100

    correct adjustment for days since last activity

--- src/projects/strava_fitness.py
+++ src/projects/strava_fitness.py
@@ -62,12 +62,12 @@ def run():
     st.subheader("Fitness simulator")
     st.caption("Workout 1")
     col1, col2, col3, col4 = st.columns(4)
-    days_from_today_1 = int(col1.text_input("Days from today", value=2, key=11)) + days_since_last_workout
+    days_from_today_1 = int(col1.text_input("Days from today", value=2, key=11))
     mov_time = col2.text_input("Moving time (minutes)", value=0, key=12)
     avg_hr = col3.text_input("Average heart rate", value=0, key=13)
     payload_ss = [[float(mov_time)*60, float(avg_hr)]]
     suffer_score_1 = eval(predictor_suffer_score.predict(payload_ss).decode())[0]
-    payload_fitness = [[ini, float(days_from_today_1), suffer_score_1]]
+    payload_fitness = [[ini, int(days_from_today_1 + days_since_last_workout), suffer_score_1]] # adjusting for total days since last activity
     fitness_score_1 = eval(predictor_fitness.predict(payload_fitness).decode())[0]
     col4.metric("Predicted suffer score", round(suffer_score_1, 0))
 
@@ -94,7 +94,7 @@ def run():
     col4.metric("Predicted suffer score", round(suffer_score_3, 0))
 
     extend_in_future = [days_from_today_1, days_from_today_2, days_from_today_3]
-    future_dates_ = np.array([np.datetime64("today") + np.timedelta64(i, 'D') for i in extend_in_future])
+    future_dates_ = np.array([np.datetime64("now") + np.timedelta64(i, 'D') for i in extend_in_future])
     future_dates = np.c_[future_dates_,future_dates_].ravel()
     future_fitness = np.round(np.array([fitness_score_1, fitness_score_2, fitness_score_3]).ravel(),2)
 
